0a11b4af6bd49a2d9af4009967cbaa16e252dbde
icvitkov <ivana.cvit@hotmail.com>
2020-01-13 22:28:18 +0100

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,26 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    "space-before-function-paren": 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 0,
+    'semi': 0,
+    'arrow-parens': [
+      2,
+      'always'
+    ],
+    'space-before-function-paren': 0,
+    'vue/order-in-components': 0,
+    'vue/html-self-closing': 0,
+    'vue/no-side-effects-in-computed-properties': 0,
+    'vue/max-attributes-per-line': [
+      0,
+      {
+        'singleline': 2,
+        'multiline': {
+          'max': 1,
+          'allowFirstLine': false
+        }
+      }
+    ]
   },
   parserOptions: {
     parser: 'babel-eslint'
